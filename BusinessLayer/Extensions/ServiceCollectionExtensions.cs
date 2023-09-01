a24814b3ee95d2b762bf252a59abfec4566b0cdd
Mathew Juiko <juikomathew@mail.ru>
2023-09-01 20:35:59 +0300

    end business layer

---
+++ BusinessLayer/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Interfaces;
+using BusinessLayer.Services;
+using DatabaseLayer.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBL(this IServiceCollection services, 
+            string connection)
+        {
+            services.AddScoped<IPartyServices, PartyService>();
+            services.AddDL(connection);
+
+            return services;
+        }
+    }
+}
