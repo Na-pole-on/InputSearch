c5509b73762af545be374e343758751238d9a42f
Mathew Juiko <juikomathew@mail.ru>
2023-09-01 19:41:16 +0300

    start database layer

---
+++ DatabaseLayer/Entities/Party.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLayer.Entities
+{
+    internal class Party
+    {
+        public string? Id { get; set; } = Guid.NewGuid().ToString();
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? PartyIdentifier { get; set; }
+    }
+}
