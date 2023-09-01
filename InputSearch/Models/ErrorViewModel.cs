7f47f69020fedb6ed14fcbfd985d375816244529
Mathew Juiko <juikomathew@mail.ru>
2023-09-01 18:27:08 +0300

    Add project files.

---
+++ InputSearch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InputSearch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
